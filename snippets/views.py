476e63d88ce8fd991696b24ebdb8836ddab20aef
Fernando Freitas Alves <ffreitasalves@gmail.com>
2015-03-02 15:07:41 -0300

    views to list and retrive users

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,8 @@
 #coding: utf-8
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 class SnippetList(generics.ListCreateAPIView):
     """
@@ -17,3 +18,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
