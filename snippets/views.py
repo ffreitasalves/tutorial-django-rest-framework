485d58281491f3c3b44735b922f802ddaa635eed
Fernando Freitas Alves <ffreitasalves@gmail.com>
2015-03-02 15:04:51 -0300

    refactoring

--- snippets/views.py
+++ snippets/views.py
@@ -1,12 +1,6 @@
 #coding: utf-8
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.shortcuts import get_object_or_404
-from rest_framework import mixins
 from rest_framework import generics
 
 class SnippetList(generics.ListCreateAPIView):
